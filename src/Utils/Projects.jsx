6577fd949049121cd4345871a137a573ac30a7be
syedtayab321 <tayabsajid5@gmail.com>
2025-05-10 11:01:23 +0500

    project added

--- src/Utils/Projects.jsx
+++ src/Utils/Projects.jsx
@@ -11,6 +11,12 @@ import pinnup3 from './../assets/projectImages/Pinnup/image3.png';
 import pinnup4 from './../assets/projectImages/Pinnup/image4.png';
 import pinnupThumbnail from './../assets/projectImages/Pinnup/thumbnail1.jpg';
 
+import carpulseMain from '../assets/projectImages/CarPulse/website1.png';
+import carpulse2 from '../assets/projectImages/CarPulse/website2.png'
+import carpulse3 from '../assets/projectImages/CarPulse/website3.png'
+import carpulse4 from '../assets/projectImages/CarPulse/website4.png'
+import carpulse5 from '../assets/projectImages/CarPulse/website5.png'
+
 const projects = [
   {
     id: "housease",
@@ -19,10 +25,10 @@ const projects = [
       "HouseEase is an innovative web and mobile application designed to streamline the process of buying, selling, and renting properties. ",
     techStack: "Flutter,Django,MySQL",
     image: houseThumbnail,
-    images: [house1, house2, house3, house4, house5],
+    images: [houseThumbnail, house2, house3, house4, house5],
     link: "/projects/carpulse",
     liveLink: "#",
-    repoLink: "#",
+    repoLink: "https://github.com/syedtayab321/shiftease_frontend",
     features: [
       "Property Listings Management ",
       "Advanced Search & Filters",
@@ -48,6 +54,25 @@ const projects = [
       "Friend Requests & Connections",
       "Notifications & Alerts",
       "Privacy & Security Settings"
+    ],
+  },
+  {
+    id: "carpulse",
+    title: " CarPulse - Your Ultimate Car Care Companion",
+    description:
+      "Keep your vehicle running smoothly with CarPulse - the one-stop solution for all your car service needs. From routine maintenance to emergency repairs.",
+    techStack: "React,JavaScript,Firebase",
+    image: carpulseMain,
+    images: [carpulseMain,carpulse2,carpulse3,carpulse4,carpulse5],
+    link: "/projects/carpulse",
+    liveLink: "https://car-pulse.vercel.app/",
+    repoLink: "https://github.com/syedtayab321/CarPulse",
+    features: [
+      "Service Management",
+      "Smart Service Search",
+      "Garage Locator Map",
+      "One-Click Booking",
+      "Maintenance Alerts",
     ],
   },
 ];
